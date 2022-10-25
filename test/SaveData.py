5d3ed12147a26188632bd890aeffd04fe471f114
Romanoff25 <mojilrmlopez@gmail.com>
2022-10-25 12:03:27 +0200

    Python_C4 - Save Data

--- test/SaveData.py
+++ test/SaveData.py
@@ -1,18 +1,34 @@
 
 #%%# Librerias
 import serial
+from os.path import exists
+#from io import StringIO
+#import pandas as pd
 
+#%%# Define
+path = r'C:\Users\usuario\OneDrive\Documentos\PlatformIO\Projects\RedesSensores_P1\test\log.csv'
 
 #%%#Inicializar Serial
 ser = serial.Serial('COM3')
 ser.baudrate = 115200
 
 #%%# Create header
-#    with open(r'.\test\log.csv','a') as f:
-#       f.write(ser.readline().decode())
+if(not(exists(path))):
+    with open(path,'x') as f:
+        f.write("axis,d0,d1,d2,d3,d4,d5,d6,d7,d8,d9\n")
 
 #%%# Escribir en el ".txt"
 while(1):
-    with open(r'.\test\log.csv','a') as f:
+    with open(path,'a') as f:
         f.write(ser.readline().decode())
 
+
+'''
+# %%
+df = pd.read_csv(StringIO(ser.readline().decode()), sep=";")
+# %%
+print(df)
+
+#%%
+print()
+'''
