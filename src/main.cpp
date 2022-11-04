3637dd4b159a37b5d7fc270657d7b5a87e8fd24e
Romanoff25 <mojilrmlopez@gmail.com>
2022-11-04 11:39:34 +0100

    Wifi/IP_C4 - Send time to server

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,47 @@
+//LIBRERIES
 #include <WiFi.h>
 #include "time.h"
 
+
+//GATEWAY DATA
 const char* ssid       = "RSense";
 const char* password   = "";
 
+//const char* ssid       = "Vendo_Opel_Corsa";
+//const char* password   = "adangorron";
+
+//SERVER
+//WiFiServer server(65432);
+
+//CLIENT
+WiFiClient client;
+IPAddress IP_Server (192,168,0,193);
+
+//NTP DATA
 const char* ntpServer = "pool.ntp.org";
 const long  gmtOffset_sec = 3600;
 const int   daylightOffset_sec = 3600;
 
-void printLocalTime()
-{
-  struct tm timeinfo;
-  if(!getLocalTime(&timeinfo)){
-    Serial.println("Failed to obtain time");
-    return;
-  }
-  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
-}
+//IP DATA
+IPAddress IP_local;
+IPAddress IP_public;
+IPAddress IP_SubnetMask;
+
+
+//
+String hostName = "www.google.com";
+
+
+
 
 void setup()
 {
   Serial.begin(115200);
-  
-  //connect to WiFi
+
+  //Numero de redes disponibles
+  Serial.println(WiFi.scanNetworks());
+
+  //Connect to WiFi
   Serial.printf("Connecting to %s ", ssid);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
@@ -30,18 +49,56 @@ void setup()
       Serial.print(".");
   }
   Serial.println(" CONNECTED");
-  
-  //init and get the time
+
+  //Save local IP
+  IP_local = WiFi.localIP();
+  IP_public = WiFi.gatewayIP();
+  IP_SubnetMask = WiFi.subnetMask();
+
+  Serial.print("Local:");  Serial.println(IP_local);
+  Serial.print("Public:"); Serial.println(IP_public);
+  Serial.print("Subnet:"); Serial.println(IP_SubnetMask);
+
+  //Actualizar Hora
   configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-  printLocalTime();
-
-  //disconnect WiFi as it's no longer needed
-  WiFi.disconnect(true);
-  WiFi.mode(WIFI_OFF);
+  
+    
 }
 
-void loop()
-{
-  delay(1000);
-  printLocalTime();
+
+//PRINT TIME LOOP
+void loop(){  
+
+
+//>>>>>>>>CLIENT<<<<<<<<<<
+  if(client.connect(IP_Server, 65432)){
+    while(1){
+
+      //PRINT TIME
+
+      struct tm timeinfo;
+      getLocalTime(&timeinfo);
+        //Data Format
+      char cData [16];
+      cData[0] = char(timeinfo.tm_hour/10 + 0x30);
+      cData[1] = char(timeinfo.tm_hour%10 + 0x30);
+      cData[2] = char(0x3a);
+      cData[3] = char(timeinfo.tm_min/10 + 0x30);
+      cData[4] = char(timeinfo.tm_min%10 + 0x30);
+      cData[5] = char(0x3a);
+      cData[6] = char(timeinfo.tm_sec/10 + 0x30);
+      cData[7] = char(timeinfo.tm_sec%10 + 0x30);
+      Serial.println(cData);
+        //Send data
+      client.write(cData);
+      client.write("\n");
+
+      delay(1000);
+    }
+    
+  }
+
 }
+
+
+
