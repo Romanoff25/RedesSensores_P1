dbe140aeb5d3c9ca4e8237f20c46137d6d8763e4
Romanoff25 <mojilrmlopez@gmail.com>
2022-12-28 00:32:58 +0100

    Bluetooth - SPP

--- src/main.cpp
+++ src/main.cpp
@@ -1,113 +1,39 @@
-// Adafruit IO Publish Example
-//
-// Adafruit invests time and resources providing this open source code.
-// Please support Adafruit and open source hardware by purchasing
-// products from Adafruit!
-//
-// Written by Todd Treece for Adafruit Industries
-// Copyright (c) 2016 Adafruit Industries
-// Licensed under the MIT license.
-//
-// All text above must be included in any redistribution.
 
-/************************** Configuration ***********************************/
-
-// edit the config.h tab and enter your Adafruit IO credentials
-// and any additional configuration needed for WiFi, cellular,
-// or ethernet clients.
-#include "config.h"
-
-/************************ Example Starts Here *******************************/
-
-// this int will hold the current count for our sketch
-int count = 0;
-
-// set up the 'counter' feed
-AdafruitIO_Feed *counter = io.feed("counter");
-
-
-// this function is called whenever a 'counter' message
-// is received from Adafruit IO. it was attached to
-// the counter feed in the setup() function above.
-void handleMessage(AdafruitIO_Data *data) {
-
-  Serial.print("received <- ");
-  Serial.println(data->value());
-
-}
-
+#include "BluetoothSerial.h"
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+BluetoothSerial BT; // Objeto Bluetooth
 
 
 void setup() {
-
-  // start the serial connection
-  Serial.begin(115200);
-
-  // wait for serial monitor to open
-  while(! Serial);
-
-  Serial.print("Connecting to Adafruit IO");
-
-  // connect to io.adafruit.com
-  io.connect();
-
-  // wait for a connection
-  while(io.status() < AIO_CONNECTED) {
-    Serial.print(".");
-    delay(500);
-  }
-
-
-
-//////////////////////////////////////////////////SUBS
-  // set up a message handler for the count feed.
-  // the handleMessage function (defined below)
-  // will be called whenever a message is
-  // received from adafruit io.
-  counter->onMessage(handleMessage);
-
-  // wait for an MQTT connection
-  // NOTE: when blending the HTTP and MQTT API, always use the mqttStatus
-  // method to check on MQTT connection status specifically
-  while(io.mqttStatus() < AIO_CONNECTED) {
-    Serial.print(".");
-    delay(500);
-  }
-
-    // Because Adafruit IO doesn't support the MQTT retain flag, we can use the
-  // get() function to ask IO to resend the last value for this feed to just
-  // this MQTT client after the io client is connected.
-  counter->get();
-
-
-
-///////////////////////////////////////////////////////
-  // we are connected
-  Serial.println();
-  Serial.println(io.statusText());
+  Serial.begin(115200); // Inicialización de la conexión en serie para la depuración
+  BT.begin("ESP32_LED_Control"); // Nombre de su dispositivo Bluetooth y en modo esclavo
+  Serial.println("El dispositivo Bluetooth está listo para emparejarse");
 }
 
+
 void loop() {
+  //RECIBIR
+  if (BT.available()){// Comprueba si recibimos algo de Bluetooth
+    Serial.print("Recibido: ");
+    while (BT.available()){
+      char incoming = BT.read(); // Lee lo que recibimos
+      if(incoming == char(13)){
+        Serial.println("");
+      }else{
+        Serial.print(incoming);
+      }
+    }
+  }
 
-  // io.run(); is required for all sketches.
-  // it should always be present at the top of your loop
-  // function. it keeps the client connected to
-  // io.adafruit.com, and processes any incoming data.
-  io.run();
+  //ENVIAR
+  if(Serial.available()){
+    BT.println(Serial.read()); // Envía el mensaje de texto a través de BT Serial
+  }
 
-  // save count to the 'counter' feed on Adafruit IO
-  Serial.print("sending -> ");
-  Serial.println(count);
-  counter->save(count);
-
-  // increment the count by 1
-  count++;
-
-  // Adafruit IO is rate limited for publishing, so a delay is required in
-  // between feed->save events. In this example, we will wait three seconds
-  // (1000 milliseconds == 1 second) during each loop.
-  delay(3000);
-
+  //DELAY
+  delay(20);
 }
 
 
