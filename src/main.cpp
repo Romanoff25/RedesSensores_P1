821bb328f879e1918310028d9e1dad6388382613
Romanoff25 <mojilrmlopez@gmail.com>
2022-12-28 20:41:02 +0100

    Bluetooth - GATT/Counter

--- src/main.cpp
+++ src/main.cpp
@@ -1,139 +1,61 @@
-/*
-   Based on 31337Ghost's reference code from https://github.com/nkolban/esp32-snippets/issues/385#issuecomment-362535434
-   which is based on pcbreflux's Arduino ESP32 port of Neil Kolban's example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleScan.cpp
-*/
-
-/*
-   Create a BLE server that will send periodic iBeacon frames.
-   The design of creating the BLE server is:
-   1. Create a BLE Server
-   2. Create advertising data
-   3. Start advertising.
-   4. wait
-   5. Stop advertising.
-*/
-#include <BLEDevice.h>
-#include <BLEServer.h>
-#include <BLEUtils.h>
-#include <BLE2902.h>
-#include <BLEBeacon.h>
+/*Program to use GATT service on ESP32 to send Battery Level
+ * ESP32 works as server - Mobile as client
+ * Program by: B.Aswinth Raj
+ * Dated on: 13-10-2018
+ * Website: www.circuitdigest.com
+ */
 #include <Arduino.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h> //Library to use BLE as server
+#include <BLE2902.h>
 
-#define DEVICE_NAME            "ESP32"
-#define SERVICE_UUID           "7A0247E7-8E88-409B-A959-AB5092DDB03E"
-#define BEACON_UUID            "RAMON"
-#define BEACON_UUID_REV        "A134D0B2-1DA2-1BA7-C94C-E8E00C9F7A2D"
-#define CHARACTERISTIC_UUID    "82258BAA-DF72-47E8-99BC-B73D7ECD08A5"
+bool _BLEClientConnected = false;
+BLECharacteristic CountCharacteristic(BLEUUID("2A19"), BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY);
+BLEDescriptor CountDescriptor(BLEUUID("2901"));
 
-BLEServer *pServer;
-BLECharacteristic *pCharacteristic;
-bool deviceConnected = false;
-uint8_t value = 0;
+class MyServerCallbacks : public BLEServerCallbacks {
 
-class MyServerCallbacks: public BLEServerCallbacks {
     void onConnect(BLEServer* pServer) {
-      deviceConnected = true;
-      Serial.println("deviceConnected = true");
+      _BLEClientConnected = true;
     };
 
     void onDisconnect(BLEServer* pServer) {
-      deviceConnected = false;
-      Serial.println("deviceConnected = false");
-
-      // Restart advertising to be visible and connectable again
-      BLEAdvertising* pAdvertising;
-      pAdvertising = pServer->getAdvertising();
-      pAdvertising->start();
-      Serial.println("iBeacon advertising restarted");
+      _BLEClientConnected = false;
     }
 };
 
-class MyCallbacks: public BLECharacteristicCallbacks {
-    void onWrite(BLECharacteristic *pCharacteristic) {
-      std::string rxValue = pCharacteristic->getValue();
-
-      if (rxValue.length() > 0) {
-        Serial.println("*********");
-        Serial.print("Received Value: ");
-        for (int i = 0; i < rxValue.length(); i++) {
-          Serial.print(rxValue[i]);
-        }
-        Serial.println();
-        Serial.println("*********");
-
-      }
-    }
-};
-
-
-void init_service() {
-  BLEAdvertising* pAdvertising;
-  pAdvertising = pServer->getAdvertising();
-  pAdvertising->stop();
-
+void InitBLE() {
+  BLEDevice::init("GATT Counter");
+  // Create the BLE Server
+  BLEServer *pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
   // Create the BLE Service
-  BLEService *pService = pServer->createService(BLEUUID(SERVICE_UUID));
+  BLEService *pCounter = pServer->createService(BLEUUID("180F"));
+  pCounter->addCharacteristic(&CountCharacteristic);
+  CountDescriptor.setValue("Percentage 0 - 100");
+  CountCharacteristic.addDescriptor(&CountDescriptor);
+  CountCharacteristic.addDescriptor(new BLE2902());
+  pServer->getAdvertising()->addServiceUUID(BLEUUID("180F"));
+  pCounter->start();
+  // Start advertising
+  pServer->getAdvertising()->start();
 
-  // Create a BLE Characteristic
-  pCharacteristic = pService->createCharacteristic(
-                      CHARACTERISTIC_UUID,
-                      BLECharacteristic::PROPERTY_READ   |
-                      BLECharacteristic::PROPERTY_WRITE  |
-                      BLECharacteristic::PROPERTY_NOTIFY
-                    );
-  pCharacteristic->setCallbacks(new MyCallbacks());
-  pCharacteristic->addDescriptor(new BLE2902());
-
-  pAdvertising->addServiceUUID(BLEUUID(SERVICE_UUID));
-
-  // Start the service
-  pService->start();
-
-  pAdvertising->start();
-}
-
-void init_beacon() {
-  BLEAdvertising* pAdvertising;
-  pAdvertising = pServer->getAdvertising();
-  pAdvertising->stop();
-  // iBeacon
-  BLEBeacon myBeacon;
-  myBeacon.setManufacturerId(0x4c00);
-  myBeacon.setMajor(5);
-  myBeacon.setMinor(88);
-  myBeacon.setSignalPower(0xc5);
-  myBeacon.setProximityUUID(BLEUUID(BEACON_UUID_REV));
-
-  BLEAdvertisementData advertisementData;
-  advertisementData.setFlags(0x1A);
-  advertisementData.setManufacturerData(myBeacon.getData());
-  pAdvertising->setAdvertisementData(advertisementData);
-
-  pAdvertising->start();
 }
 
 void setup() {
   Serial.begin(115200);
-  Serial.println();
-  Serial.println("Initializing...");
-  Serial.flush();
-
-  BLEDevice::init(DEVICE_NAME);
-  pServer = BLEDevice::createServer();
-  pServer->setCallbacks(new MyServerCallbacks());
-
-  init_service();
-  init_beacon();
-
-  Serial.println("iBeacon + service defined and advertising!");
+  Serial.println("Counter - GATT");
+  InitBLE();
 }
 
+uint8_t counter = 0;
 void loop() {
-  if (deviceConnected) {
-    Serial.printf("*** NOTIFY: %d ***\n", value);
-    pCharacteristic->setValue(&value, 1);
-    pCharacteristic->notify();
-    value++;
-  }
-  delay(2000);
+  CountCharacteristic.setValue(&counter,1);
+  CountCharacteristic.notify();
+  delay(1000);
+  counter++;
+  Serial.println(int(counter));
+
 }
+
